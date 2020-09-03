6c359b12e4412013fbdce5fac7036bfa4d3f7911
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-09-03 23:08:14 +0900

    Updated Get Ranking methods

--- app/Http/Controllers/Quiz/RankingController.php
+++ app/Http/Controllers/Quiz/RankingController.php
@@ -18,6 +18,32 @@ class RankingController extends Controller
 
     public function index()
     {
+        $weekRanking = Ranking::with('user')
+            ->select(DB::raw('MAX(rankings.percentage_correct_answer) as percentage_correct_answer, rankings.user_id, rankings.created_at'))
+            ->whereBetween('rankings.created_at', [now()->startOfWeek()->format('Y-m-d'), now()->endOfWeek()->format('Y-m-d')])
+            ->limit(5)
+            ->orderby('percentage_correct_answer', 'desc')
+            ->groupBy('rankings.user_id')
+            ->get();
+
+        $weekRankingData = [
+            'percentage_correct_answer' => $weekRanking->pluck('percentage_correct_answer')->all(),
+            'username' => $weekRanking->pluck('user.username')->all()
+        ];
+
+        $monthRanking = Ranking::with('user')
+            ->select(DB::raw('MAX(rankings.percentage_correct_answer) as percentage_correct_answer, rankings.user_id, rankings.created_at'))
+            ->whereBetween('rankings.created_at', [now()->startOfMonth()->format('Y-m-d'), now()->endOfMonth()->format('Y-m-d')])
+            ->limit(5)
+            ->orderby('percentage_correct_answer', 'desc')
+            ->groupBy('rankings.user_id')
+            ->get();
+
+        $monthRankingData = [
+            'percentage_correct_answer' => $monthRanking->pluck('percentage_correct_answer')->all(),
+            'username' => $monthRanking->pluck('user.username')->all()
+        ];
+
         $totalRanking = Ranking::with('user')
             ->select(DB::raw('MAX(rankings.percentage_correct_answer) as percentage_correct_answer, rankings.user_id, rankings.created_at'))
             ->limit(5)
@@ -27,10 +53,10 @@ class RankingController extends Controller
 
         $totalRankingData = [
             'percentage_correct_answer' => $totalRanking->pluck('percentage_correct_answer')->all(),
-            'name' => $totalRanking->pluck('user.name')->all()
+            'username' => $totalRanking->pluck('user.username')->all()
         ];
 
-        return response()->json($totalRankingData, 200);
+        return response()->json([$weekRankingData, $monthRankingData, $totalRankingData], 200);
     }
 
     public function insertRanking(Request $request)
